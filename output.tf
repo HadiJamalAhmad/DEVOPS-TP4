b16dc432e0042b2144661e41a156074d68adcac2
Hadi Jamal Ahmad <33461956+HadiJamalAhmad@users.noreply.github.com>
2022-06-30 10:29:39 +0200

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,18 @@
+output "resource_group_name" {
+  value = data.azurerm_resource_group.tp4.name
+}
+
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.devops-20211136.public_ip_address
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,7 @@
+
+
+variable "region" {
+  default       = "france central"
+  description   = "Location of the resource group."
+}
+
